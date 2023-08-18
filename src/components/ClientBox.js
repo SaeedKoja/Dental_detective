3415513dbb5c75c67e21a2d0ebc328817c51f624
SaeedKoja <saeedkojastudy@gmail.com>
2023-08-18 14:06:47 +0300

    complaints page

--- src/components/ClientBox.js
+++ src/components/ClientBox.js
@@ -4,21 +4,25 @@ import cancel from '../assets/icons/cancel.png';
 import location from '../assets/icons/location.png';
 import tel from '../assets/icons/phone-call.png';
 
-const ClientBox = ({doctor,onDelete}) => {
+const ClientBox = ({doctor,onDelete,page='clients'}) => {
     return (
         <div className='relative'>
-            <div className='client flex flex-col items-center py-4 pt-10 border-[2px] rounded-l-[18px] rounded-r-[18px] border-[var(--border-color)]'>
+            <div className='client flex flex-col items-center py-4 px-3 pt-10 border-[2px] rounded-l-[18px] rounded-r-[18px] border-[var(--border-color)]'>
                 <img className='w-[30px] mb-7' src={user}></img>
                     <p className='text-xl font-bold text-[var(--dark-color)]'>{doctor.name}</p>
                 <p className='font-light text-[var(--border-color)]'>{doctor.email}</p>
                 <div className='flex flex-col justify-start'> <div className='mb-2 flex items-center justify-start mt-5'>
                     <img className='w-[20px] mr-3' src={location}></img>
-                    <p className='text-[var(--dark-color)]'>{doctor.location}</p>
+                    <p className='text-[var(--dark-color)] font-light'>{doctor.location}</p>
                 </div>
                     <div className='flex items-center'>
                         <img className='w-[20px] mr-3' src={tel}></img>
-                        <p className='text-[var(--dark-color)]'>{doctor.phone}</p>
+                        <p className='text-[var(--dark-color)] font-light'>{doctor.phone}</p>
                     </div></div>
+                    {page ==='complaints' && <div className='mt-5 flex flex-col items-center'>
+                            <p className='mb-2 text-lg font-semibold text-[var(--dark-color)]'>{doctor.status}</p>
+                            <p className='text-center text-sm opacity-[0.8] font-light text-[var(--dark-color)]'>{doctor.description}</p>
+                        </div>}
                 <img className='w-[30px] mt-7 cursor-pointer' onClick={() => onDelete(3)} src={cancel}></img>
             </div>
         </div>
--- src/pages/Complaints/Complaints.js
+++ src/pages/Complaints/Complaints.js
@@ -2,17 +2,128 @@ import React from 'react';
 import { useDispatch } from "react-redux";
 import { activeAction } from '../../store/active-ui';
 import { useEffect } from 'react';
+import { useState } from 'react';
+import DeleteItem from '../../components/DeleteItem';
+import ClientBox from '../../components/ClientBox';
 
 const Complaints = () => {
     const dispatch = useDispatch();
+    const [del, setDel] = useState();
+    const [clientId, setClientId] = useState("");
+    const data = [
+        {
+            name: 'Fadi Awad',
+            email: 'fadidoctor@gmail.com',
+            location: 'damascus - syria',
+            phone: '0936286430',
+            status: 'complaint',
+            description: '70% of the composition of our body is water. It’s an essential element to keep the body healthy because it cleans the body',
+        },
+        {
+            name: 'Saeed Koja',
+            email: 'saeedkoja@gmail.com',
+            location: 'homs - syria',
+            phone: '0936284530',
+            status: 'complaint',
+            description: '70% of the composition of our body is water. It’s an essential element to keep the body healthy because it cleans the body',
+        },
+        {
+            name: 'Ahmad Kurdy',
+            email: 'ahmaddoctor@gmail.com',
+            location: 'damascus - syria',
+            phone: '0936286430',
+            status: 'complaint',
+            description: '70% of the composition of our body is water. It’s an essential element to keep the body healthy because it cleans the body',
+        },
+        {
+            name: 'Fadi Awad',
+            email: 'fadidoctor@gmail.com',
+            location: 'damascus - syria',
+            phone: '0936286430',
+            status: 'complaint',
+            description: '70% of the composition of our body is water. It’s an essential element to keep the body healthy because it cleans the body',
+        },
+        {
+            name: 'Fadi Awad',
+            email: 'fadidoctor@gmail.com',
+            location: 'damascus - syria',
+            phone: '0936286430',
+            status: 'complaint',
+            description: '70% of the composition of our body is water. It’s an essential element to keep the body healthy because it cleans the body',
+        },
+    ]
 
-    useEffect(()=>{
+    const deleteClientHandler = (id) => {
+        setClientId(id);
+        setDel(true);
+    };
+
+    const confrimHandler = () => {
+        // axios
+        //     .delete(`${API.consultations.CONSULTATIONS}${conId}`, {
+        //         headers: {
+        //             Authorization: "JWT " + Cookies.get("accessToken")
+        //         }
+        //     })
+        //     .then((res) => {
+        //         console.log(res);
+        //         setDel(false);
+        //         setConsultations((prev) => {
+        //             return {
+        //                 ...prev,
+        //                 count: consultations.count - 1,
+        //                 pending_count: isPending
+        //                     ? consultations.pending_count - 1
+        //                     : consultations.pending,
+        //                 data: consultations.data.filter((array) => array.id !== conId)
+        //             };
+        //         });
+        //     });
+    };
+
+
+    useEffect(() => {
         dispatch(activeAction.replaceActiveState('Complaints'))
-    },[])
+    }, [])
+
+    const searchHandler = (e) => { }
 
     return (
         <div className='pl-[300px] pr-5 py-5'>
-            Complaints
+            {del && (
+                <DeleteItem onConfrim={confrimHandler} onBack={() => setDel(false)} />
+            )}
+            <div className='mb-16 flex justify-between items-center '>
+                <p className='text-[var(--blak-color)] text-4xl font-bold'>Complaints from clients</p>
+                <div className="relative">
+                    <input
+                        type="search"
+                        placeholder="search for a specific client"
+                        className="shadow-md w-[500px] border border-[var(--gray-color)] rounded-l-full rounded-r-full py-2 pr-2 pl-12 bg-inherit"
+                        name="searsh"
+                        onChange={searchHandler}
+                    />
+                    <div className="absolute top-[10px] left-4 opacity-[0.6]">
+                        <box-icon name="search" color="var(--gray-color)"></box-icon>
+                    </div>
+                </div>
+            </div>
+            <div
+                className="mx-20"
+                style={{
+                    display: "grid",
+                    gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+                    gap: "30px",
+                    margin: "auto",
+                    maxWidth: "100%",
+                }}
+            >
+                {data.map((doctor, index) => {
+                    return (
+                        <ClientBox page='complaints' doctor={doctor} key={index} onDelete={deleteClientHandler} />
+                    )
+                })}
+            </div>
         </div>
     );
 }
