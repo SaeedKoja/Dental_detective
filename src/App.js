dd7cd38088f049c88ab7841a1de01214e0e2663d
SaeedKoja <saeedkojastudy@gmail.com>
2023-08-22 23:44:30 +0300

    handling error

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import React from "react";
 import Dental from "./pages/Dental/Dental";
 import Login from "./pages/Auth/Login"
-import Register from "./pages/Auth/Register"
 import RedirectPage from "./pages/Auth/RedirectPage"
-import CodeVerification from "./pages/Auth/CodeVerification"
-import ChangePassword from "./pages/Auth/ChangePassword"
-import ResetPassword from "./pages/Auth/ResetPassword"
-import ForgotPassword from "./pages/Auth/ForgotPassword"
 import Home from "./pages/Home/Home"
 import SendComplaints from "./pages/SendComplaints/SendComplaints";
 import Complaints from "./pages/Complaints/Complaints";
@@ -25,22 +20,6 @@ function App() {
       <Routes>
         <Route path="/" element={<RedirectPage />}></Route>
         {!isLogged && <Route path="/Login" element={<Login />}></Route>}
-        {/* {!isLoggedIn && <Route path="/Register" element={<Register />}></Route>} */}
-        {/* {!isLoggedIn && (
-          <Route
-            path="/CodeVerification"
-            element={<CodeVerification />}
-          ></Route>
-        )} */}
-        {/* {isLoggedIn && (
-          <Route path="/Change_password" element={<ChangePassword />}></Route>
-        )} */}
-        {/* {!isLoggedIn && (
-          <Route path="/Reset_password" element={<ResetPassword />}></Route>
-        )} */}
-        {/* {!isLoggedIn && (
-          <Route path="/ForgotPassword" element={<ForgotPassword />}></Route>
-        )} */}
         {isLogged && (
           <Route path="/Dental" element={<Dental />}>
             <Route path="Home" element={<Home />}></Route>
