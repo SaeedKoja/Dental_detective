3fc1d41d998e0bd323e03c2b2a3686301b88a0c4
SaeedKoja <saeedkojastudy@gmail.com>
2023-08-23 11:25:50 +0300

    remove delete case

--- src/components/productBox.js
+++ src/components/productBox.js
@@ -5,8 +5,8 @@ const ProductBox = ({item,onDelete}) => {
     return (
         <div className='flex flex-col items-center py-4 px-2 border-[2px] rounded-l-[18px] rounded-r-[18px] bg-white shadow-sm border-[var(--border-color)]'>
             <img className='w-[194px] h-[194px] mb-5 rounded-md' src={item.image} alt=''></img>
-            <p className='text-xl font-bold text-[var(--dark-color)]'>{item.comment}</p>
-            <img className='w-[35px] mt-4 cursor-pointer' onClick={() => onDelete(item.id)} alt='' src={cancel}></img>
+            <p className='text-xl font-bold mb-4 text-[var(--dark-color)]'>{item.comment}</p>
+            {/* <img className='w-[35px] mt-4 cursor-pointer' onClick={() => onDelete(item.id)} alt='' src={cancel}></img> */}
         </div>
     );
 }
